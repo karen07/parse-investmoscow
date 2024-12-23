67e9ad214ade39b081b94290bb2c36ca22759854
Karen <karen0734@gmail.com>
2024-12-23 08:45:09 +0000

    Make exec

--- desp.sh
+++ desp.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-sudo pacman -Syu nodejs npm base-devel git nss at-spi2-core libcups libdrm libxcomposite libxdamage libxrandr mesa libxkbcommon pango alsa-lib vim less
+if [ -f /usr/bin/pacman ]; then
+	sudo pacman -S nodejs npm base-devel git nss at-spi2-core libcups libdrm \
+		libxcomposite libxdamage libxrandr mesa libxkbcommon pango alsa-lib vim less --noconfirm
+fi
