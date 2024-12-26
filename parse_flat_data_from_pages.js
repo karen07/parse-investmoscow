b9d2efc6ff368dea89d39515a1a1df75d8f07f60
Karen <karen0734@gmail.com>
2024-12-26 14:00:07 +0000

    Update out data

--- parse_flat_data_from_pages.js
+++ parse_flat_data_from_pages.js
@@ -10,18 +10,15 @@ async function asyncCall() {
 	let renovation_coordinate = [];
 	require('fs').readFileSync("renovation_coordinates.txt", 'utf-8').split(/\r?\n/).forEach(function (line) {
 		if (line != '') {
-			renovation_coordinate.push({ x: line.split(";")[1], y: line.split(";")[2] });
+			renovation_coordinate.push({ x: line.split(",")[0], y: line.split(",")[1] });
 		}
 	})
 
-	//console.log(renovation_coordinate);
-
-	const pages = fs.readdirSync("pages/");
-	//console.log("tenderId;viewsCount;lat;long;addreess;start price;end price;data;area;div;price/area");
+	const pages = fs.readdirSync("data/");
 	console.log("tenderId;viewsCount;addreess;start price;end price;data;area;div;price/area");
 	for (const i of pages) {
 		lines = '';
-		require('fs').readFileSync("pages/" + i, 'utf-8').split(/\r?\n/).forEach(function (line) {
+		require('fs').readFileSync("data/" + i, 'utf-8').split(/\r?\n/).forEach(function (line) {
 			if (line != '') {
 				lines += line;
 			}
@@ -44,19 +41,16 @@ async function asyncCall() {
 					let y = obj["mapInfo"]["coords"]["long"];
 					let dist = Math.sqrt((j.x - x) * (j.x - x) + (j.y - y) * (j.y - y));
 					if (dist < 0.0005) {
-						//console.log(dist);
 						console.log(
 							obj["tenderId"], ";",
 							obj["headerInfo"]["viewsCount"], ";",
-							//obj["mapInfo"]["coords"]["lat"], ";",
-							//obj["mapInfo"]["coords"]["long"], ";",
 							obj["mapInfo"]["address"].replaceAll(";", " ").replaceAll("\n", " "), ";",
 							obj["mapInfo"]["price"].toString().split(" ")[0].split(",")[0].split(".")[0], ";",
 							names_map.get("Итоговая цена").toString().split(" ")[0].split(",")[0].split(".")[0], ";",
 							names_map.get("Подведение итогов").split(" ")[0], ";",
 							names_map.get("Площадь объекта").toString().split(" ")[0].split(",")[0].split(".")[0], ";",
-							((parseInt(names_map.get("Итоговая цена").toString().split(" ")[0].split(",")[0].split(".")[0]) / parseInt(obj["mapInfo"]["price"].toString().split(" ")[0].split(",")[0].split(".")[0]) - 1) * 100).toString().replace(".", ","), ";",
-							(names_map.get("Итоговая цена").toString().split(" ")[0].split(",")[0].split(".")[0] / names_map.get("Площадь объекта").toString().split(" ")[0].split(",")[0].split(".")[0]).toString().replace(".", ","), ";"
+							Math.ceil((parseInt(names_map.get("Итоговая цена").toString().split(" ")[0].split(",")[0].split(".")[0]) / parseInt(obj["mapInfo"]["price"].toString().split(" ")[0].split(",")[0].split(".")[0]) - 1) * 100), ";",
+							Math.ceil((names_map.get("Итоговая цена").toString().split(" ")[0].split(",")[0].split(".")[0]) / (names_map.get("Площадь объекта").toString().split(" ")[0].split(",")[0].split(".")[0])), ";"
 						);
 						break;
 					}
