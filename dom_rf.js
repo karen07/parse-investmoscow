b7743e2cad0b91a4b751686d4ffd999f60079bd8
Khachatryan Karen <karen0734@gmail.com>
2025-02-03 20:29:47 +0300

    Add colors file

--- dom_rf.js
+++ dom_rf.js
@@ -9,18 +9,12 @@ function sleep(ms) {
 }
 
 async function asyncCall(thread) {
-    let url_stock = 'https://xn--80az8a.xn--d1aqf.xn--p1ai/' +
-        '/%D1%81%D0%B5%D1%80%D0%B2%D0%B8%D1%81%D1%8B' +
-        '/%D0%BA%D0%B0%D1%82%D0%B0%D0%BB%D0%BE%D0%B3-%D0%BD%D0%BE%D0%B2%D0%BE%D1%81%D1%82%D1%80%D0%BE%D0%B5%D0%BA' +
-        '/%D1%81%D0%BF%D0%B8%D1%81%D0%BE%D0%BA-%D0%BE%D0%B1%D1%8A%D0%B5%D0%BA%D1%82%D0%BE%D0%B2' +
-        '/%D1%81%D0%BF%D0%B8%D1%81%D0%BE%D0%BA?' +
+    let url_stock = 'https://наш.дом.рф/сервисы/каталог-новостроек/список-объектов/список?' +
         'place=77' +
         '&objStatus=0' +
         '&toQuarter=2027-12-31' +
         '&residentialBuildings=1' +
         '&escrowFlag=1';
-
-    //console.log(url_stock);
 
     const browser = await puppeteer.launch({
         headless: false
