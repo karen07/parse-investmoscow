f4be0b1fc44a2c30bb4fc0827a0abb7fe9bfcfdc
Khachatryan Karen <karen0734@gmail.com>
2025-02-03 21:24:47 +0300

    Update desp.sh

--- desp.sh
+++ desp.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
 if [ -f /usr/bin/pacman ]; then
-	sudo pacman -S nodejs npm base-devel git nss at-spi2-core libcups libdrm \
+	sudo pacman -S base-devel git nss at-spi2-core libcups libdrm \
 		libxcomposite libxdamage libxrandr mesa libxkbcommon pango alsa-lib vim less --noconfirm
 fi
+
+if [ -f /usr/bin/apt ]; then
+	sudo apt update
+	sudo apt install libnss3 unzip libasound2t64 -y
+fi
+
+curl -o- https://fnm.vercel.app/install | bash
+
+# fnm install 23
