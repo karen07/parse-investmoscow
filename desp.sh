c4cc0e55946a463b9116c01312f41447c9689c17
Khachatryan Karen <karen0734@gmail.com>
2025-07-28 10:57:38 +0300

    Format sh

--- desp.sh
+++ desp.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 if [ -f /usr/bin/pacman ]; then
-	sudo pacman -S base-devel git nss at-spi2-core libcups libdrm \
-		libxcomposite libxdamage libxrandr mesa libxkbcommon pango alsa-lib vim less --noconfirm
+    sudo pacman -S base-devel git nss at-spi2-core libcups libdrm \
+        libxcomposite libxdamage libxrandr mesa libxkbcommon pango alsa-lib vim less --noconfirm
 fi
 
 if [ -f /usr/bin/apt ]; then
-	sudo apt update
-	sudo apt install libnss3 unzip libasound2t64 -y
+    sudo apt update
+    sudo apt install libnss3 unzip libasound2t64 -y
 fi
 
 curl -o- https://fnm.vercel.app/install | bash
