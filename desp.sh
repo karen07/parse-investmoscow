5dff85562f8e91039fffa89aaf6e979ba71d6ce7
Khachatryan Karen <karen0734@gmail.com>
2025-08-10 15:58:32 +0300

    Fix deps

--- desp.sh
+++ desp.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
 if [ -f /usr/bin/pacman ]; then
-    sudo pacman -S base-devel git nss at-spi2-core libcups libdrm \
-        libxcomposite libxdamage libxrandr mesa libxkbcommon pango alsa-lib vim less --noconfirm
+    sudo pacman -Sy --noconfirm base-devel git nss at-spi2-core libcups libdrm libxcomposite libxdamage libxrandr mesa libxkbcommon pango alsa-lib vim less
 fi
 
 if [ -f /usr/bin/apt ]; then
     sudo apt update
-    sudo apt install libnss3 unzip libasound2t64 -y
+    sudo apt install -y libnss3 unzip libasound2t64
 fi
 
 curl -o- https://fnm.vercel.app/install | bash
